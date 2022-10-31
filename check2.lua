491eced8ceed09c9576b17cc4df70f8de89262c9
ESX Developer Portugal <88593228+zcmg@users.noreply.github.com>
2022-10-31 18:06:43 +0000

    Update check2.lua

--- check2.lua
+++ check2.lua
@@ -1,29 +1,28 @@
-PerformHttpRequest('https://raw.githubusercontent.com/zcmg/'..GetCurrentResourceName()..'/main/fxmanifest.lua', function(code, res, headers)
-	local vv = GetResourceMetadata(GetCurrentResourceName(), 'description')
-	local p = string.sub(GetCurrentResourceName(),1, 4)
-	local v = ''
-	local u = ''
+PerformHttpRequest('https://raw.githubusercontent.com/zcmg/'..GetCurrentResourceName()..'/main/fxmanifest.lua', function(code, r, headers)
+	local vv, p, v, u, c = GetResourceMetadata(GetCurrentResourceName(), 'description'), string.sub(GetCurrentResourceName(),1, 4), '', '', false
+		
+	if p ~= 'zcmg' then c = true end
 
-	if p ~= 'zcmg' then os.exit() end
-
-	if res ~= nil then
-		local t = {}
-		for i = 1, #res do
-			t[i] = res:sub(i, i)
-		end
-		v = t[73]..t[74]..t[75]..t[76]
-
-		if v == vv then
-			u = "Ultima Versão"
-		else
-			u = "^2Precisa de atualizar^1"
-		end
-
+	if c then
+		u = "^2Impossivel startar o script^1
 	else
-		u = "Impossivel verificar a versão"
-	end
+		if r ~= nil then
+			local t = {}
+			for i = 1, #res do
+				t[i] = r:sub(i, i)
+			end
+			v = t[73]..t[74]..t[75]..t[76]
 
-	
+			if v == vv then
+				u = "^2Ultima Versão^1"
+			else
+				u = "^2Precisa de atualizar^1"
+			end
+
+		else
+			u = "^2Impossivel verificar a versão^1"
+		end
+	end
 
 	print(([[^1--------------------------------------------------------------------------
 	███████╗ ██████╗███╗   ███╗ ██████╗      ██████╗ ██████╗ ██████╗ ███████╗
@@ -36,7 +35,9 @@ PerformHttpRequest('https://raw.githubusercontent.com/zcmg/'..GetCurrentResource
 	
 	--------------------------------------------------------------------------
 	-- ESX DEVELOPER PORTUGAL (^0https://discord.gg/Qt5WraEMxf^1)
-	-- Versão: %s (%s)
+	-- Versão: ^2%s^1 (%s)
 	--------------------------------------------------------------------------^0]]):format(v, u))
+		
+	if c then os.exit() end
 
 end,'GET')
