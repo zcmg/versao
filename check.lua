f918de6129d9126bdc537f4f875c89647534dcc7
ESX Developer Portugal <88593228+zcmg@users.noreply.github.com>
2022-11-01 12:35:33 +0000

    Update check.lua

--- check.lua
+++ check.lua
@@ -1,30 +1,26 @@
-PerformHttpRequest('https://raw.githubusercontent.com/zcmg/'..GetCurrentResourceName()..'/main/fxmanifest.lua', function(code, res, headers)
-	local version = GetResourceMetadata(GetCurrentResourceName(), 'description')
-	local prefixo = string.sub(GetCurrentResourceName(),1, 4)
-	local versao = ''
-	local update = ''
+PerformHttpRequest('https://raw.githubusercontent.com/zcmg/'..GetCurrentResourceName()..'/main/fxmanifest.lua', function(code, r, headers)
+	local vv, p, v, u, c = GetResourceMetadata(GetCurrentResourceName(), 'description'), string.sub(GetCurrentResourceName(),1, 4), '', '', false
+		
+	if p ~= 'zcmg' then c = true end
 
-	if prefixo ~= 'zcmg' then os.exit() end
-
-	if res ~= nil then
-		local t = {}
-		for i = 1, #res do
-			t[i] = res:sub(i, i)
-		end
-		versao = t[73]..t[74]..t[75]..t[76]
-
-		if versao == version then
-			update = "Ultima Versão"
-		else
-			update = "^2Precisa de atualizar^1"
-		end
-
+	if c then
+		u = "^2Impossivel mudar o nome do resource^1)\n -- (^0O script deu trabalho a desenvolver o unica coisa que peço é que mantenham o nome do resource^1"
 	else
-		update = "Impossivel verificar a versão"
+		if r ~= nil then
+			local t = {}
+			for i = 1, #r do
+				t[i] = r:sub(i, i)
+			end
+			v = t[73]..t[74]..t[75]..t[76]
+			if v == vv then
+				u = "^2Ultima Versão^1"
+			else
+				u = "^2Precisa de atualizar^1"
+			end
+		else
+			u = "^2Impossivel verificar a versão^1"
+		end
 	end
-
-	
-
 	print(([[^1--------------------------------------------------------------------------
 	███████╗ ██████╗███╗   ███╗ ██████╗      ██████╗ ██████╗ ██████╗ ███████╗
 	╚══███╔╝██╔════╝████╗ ████║██╔════╝     ██╔════╝██╔═══██╗██╔══██╗██╔════╝
@@ -36,7 +32,8 @@ PerformHttpRequest('https://raw.githubusercontent.com/zcmg/'..GetCurrentResource
 	
 	--------------------------------------------------------------------------
 	-- ESX DEVELOPER PORTUGAL (^0https://discord.gg/Qt5WraEMxf^1)
-	-- Versão: %s (%s)
-	--------------------------------------------------------------------------^0]]):format(versao, update))
-
+	-- Versão: ^0%s^1 (%s)
+	--------------------------------------------------------------------------^0]]):format(v, u))
+	Wait(100)
+	if c then os.exit() end
 end,'GET')
